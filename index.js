35283ec0eb4fc686cf37c5ab311563cf7b84a04b
vanshkr <krvansh30@gmail.com>
2023-04-24 15:28:01 +0530

    .

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/", sampleRoutes);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||5000;
 const fileContents = fs.readFileSync("sample_data.json");
 const data = JSON.parse(fileContents);
 
 mongoose
-  .connect(process.env.CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(PORT, () => {
       console.log(`Example app listening on PORT ${PORT}`);
