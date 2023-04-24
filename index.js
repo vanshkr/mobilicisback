d862c4badd9c599c32f5babdb037d4546dfa9e87
vanshkr <krvansh30@gmail.com>
2023-04-24 14:21:26 +0530

    changes in port

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/", sampleRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const fileContents = fs.readFileSync("sample_data.json");
 const data = JSON.parse(fileContents);
 
